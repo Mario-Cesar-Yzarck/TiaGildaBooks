678cf6f373333b744b0f83fb0d8f162c0635abcd
Mario <mc.fialho@hotmail.com>
2022-03-17 15:44:16 -0300

    first using test

--- main.js
+++ main.js
@@ -26,7 +26,6 @@ const bookCode = document.getElementById('bookCode')
 const searchTitle = document.getElementById('searchTitle')
 const searchAuthor = document.getElementById('searchAuthor')
 const searchCode = document.getElementById('searchCode')
-let counter = 0
 
 btnSubmit.addEventListener('click', e => {
     e.preventDefault();
@@ -41,107 +40,41 @@ btnSubmit.addEventListener('click', e => {
     
      title.value = ''
      author.value = ''
-     bookCode.value = ''
+     bookCode.value = ''   
     
-    /*
-    let objBook = {
-        title: "",
-        author: "",
-        code: ""
-    }
-    if(title.value && author.value && bookCode.value) {
-        objBook.title = title.value;
-        objBook.author = author.value;
-        objBook.code = bookCode.value
-        arrayBooks.push(objBook)
-       
-        alert('Livro Cadastrado com Sucesso!!')
-    }
-    title.value = "";
-    author.value = "";
-    bookCode.value = "";
-*/
 })
 
 btnSearch.addEventListener('click', e => {
-    e.preventDefault();
+    e.preventDefault();    
+    const listBooks = document.querySelector('#info');
+    listBooks.innerHTML = ''
 
     getDocs(collectionBooks)
     .then( querySnapshot => {
         querySnapshot.docs.forEach(doc => {
             const {title, author, code } = doc.data()   
-            
+            let html = ''
+
             let someTitle = title.toUpperCase() === searchTitle.value.toUpperCase();
             let someAuthor = author.toUpperCase() === searchAuthor.value.toUpperCase();
-            let someCode = code.toUpperCase() === searchCode.value.toUpperCase();
-            let html = ''              
+            let someCode = code.toUpperCase() === searchCode.value.toUpperCase();                          
             
-            if(someTitle || someAuthor || someCode) {
-                
+            if(someTitle || someAuthor || someCode) {                
                 html += `<ul>
                 <li>Título: ${title}</li>
                 <li>Autor: ${author}</li>
                 <li>Código: ${code}</li>
                 </ul>`            
-            }           
+            }   
 
-            const listBooks = document.querySelector('#info');
-            listBooks.innerHTML += html;
-
-            counter += 1
-            console.log(querySnapshot.docs.length === counter)
-            if(querySnapshot.docs) {
-                searchAuthor.value = '';
-                searchCode.value = '';
-                searchTitle.value = '';
-            }       
-        })
-         /*
-        const book = querySnapshot.docs.reduce((acc, doc) => {
-            const { title, author, code } = doc.data()
-            let someTitle = title.toUpperCase() === searchTitle.value.toUpperCase();
-            let someAuthor = author.toUpperCase() === searchAuthor.value.toUpperCase();
-            let someCode = code.toUpperCase() === searchCode.value.toUpperCase();
-              
-            if(someTitle || someAuthor || someCode) {
-                acc = `<ul>
-                <li>Título: ${title}</li>
-                <li>Autor: ${author}</li>
-                <li>Código: ${code}</li>
-                </ul>`
-                
-                return acc;            
-            }                   
-        }, '')
-
-        const listBooks = document.querySelector('#info');
-        listBooks.innerHTML = book;
-        searchAuthor.value = ''
-        searchCode.value = ''
-        searchTitle.value = ''
-        */
-        
-        
+            listBooks.innerHTML += html;            
+        })               
+        searchAuthor.value = '';
+        searchCode.value = '';
+        searchTitle.value = '';
     })
-    .catch(console.log())
-    
-/*
-    let html = ""     
-    for(let i = 0; i < arrayBooks.length; i++) {
-        if(arrayBooks[i].title === searchTitle.value || arrayBooks[i].author === searchAuthor.value || arrayBooks[i].code === searchCode.value) {            
-            html += `<ul><li>Título: ${arrayBooks[i].title}</li>`
-            html += `<li>Autor: ${arrayBooks[i].author}</li>`
-            html += `<li>Código: ${arrayBooks[i].code}</li></ul><br>`
-        } else {
-            alert('Informações não encontradas.')
-        }
-    }
-    let listaBooks = document.getElementById('info')
-    listaBooks.innerHTML = html
-    searchTitle.value = "";
-    searchAuthor.value = "";
-    searchCode.value = "";
-    */
+    .catch(console.log())  
+
 })
 
 
