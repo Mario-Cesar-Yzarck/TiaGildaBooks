0d552829c9aaf61ff7dffd21082cc0f0c9a2edbe
Mario <mc.fialho@hotmail.com>
2022-04-22 11:36:09 -0300

    update navbar

--- main.js
+++
@@ -1,8 +0,0 @@
-const btnBackToTop = document.getElementById('btnBackToTop')
-
-if(btnBackToTop) {
-    btnBackToTop.addEventListener('click', e => {
-        e.preventDefault()
-        window.scrollTo(0, 0);
-    });  
-}
